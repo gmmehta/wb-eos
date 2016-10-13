cc093a8919189b3706b1e90aa5e9c2b806ef7579
Gautam Mehta <gautam@gautam-mac-book-pro.local>
2016-10-13 16:37:22 +0100

    Initial project check in

---
+++ src/main/java/com/devtest/WbEosApplication.java
@@ -0,0 +1,12 @@
+package com.devtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbEosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbEosApplication.class, args);
+	}
+}
